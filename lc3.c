58cb8bd9df0b1d5b2ea20438252a11b8ccea0e23
kumail074 <kumail074@proton.me>
2024-11-25 23:38:06 +0500

    basic implementation

--- lc3.c
+++ lc3.c
@@ -12,7 +12,7 @@
 #include <sys/mman.h>
 
 // memory mapped registers
-enum {
+enum reg {
     R0 = 0,
     R1,
     R2,
@@ -21,7 +21,7 @@ enum {
     R5,
     R6,
     R7,
-    R_PC,
+    R_PC,    /* program counter */
     R_COND,
     R_COUNT
 };
@@ -72,4 +72,80 @@ uint16_t reg[R_COUNT];
 // read image 
 // memory access
 // main loop
+int main(int argc, char *argv[]) {
 
+    /* load arguments */
+    if(argc < 2) {
+        printf("lc3 [image-file1]...\n");
+        exit(2);
+    }
+
+    for(int j = 1; j < argc; ++j)
+    {
+        if(!read_image(argv[j])) {
+            printf("failed to load image: %s\n", argv[j]);
+            exit(1);
+        }
+    }
+    /* setup */
+
+    reg[R_COND] = FL_ZRO;
+
+    enum {PC_START = 0x3000 };
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while(running) {
+        uint16_t instr = mem_str(reg[R_PC]++);
+        uint16_t op = instr >> 12;
+        switch(op) {
+            case OP_ADD:
+                //add
+                break;
+            case OP_AND:
+                //and
+                break;
+            case OP_NOT:
+                //not
+                break;
+            case OP_BR:
+                //br
+                break;
+            case OP_JMP:
+                //jmp
+                break;
+            case OP_JSR:
+                //jsr
+                break;
+            case OP_LD:
+                //ld
+                break;
+            case OP_LDI:
+                //ldi
+                break;
+            case OP_LDR:
+                //ldr
+                break;
+            case OP_LEA:
+                //lea
+                break;
+            case OP_ST:
+                //st
+                break;
+            case OP_STI:
+                //sti
+                break;
+            case OP_STR:
+                //str
+                break;
+            case OP_TRAP:
+                //trap
+                break;
+            case OP_RES:
+            case OP_RTI:
+            default:
+                //bad opcode
+                break;
+        }
+    }
+}
